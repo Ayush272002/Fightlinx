7b41e005ff9ce8cd1e5badbe139ac0b1a8f30d8b
Ayush Acharjya <ayushacharjya@gmail.com>
2024-10-20 13:19:31 +0100

    add ci/cd pipeline + docker file for mm and backend

---
+++ docker/Dockerfile.mm
@@ -0,0 +1,17 @@
+FROM node:20.12.0-alpine3.19
+
+WORKDIR /app
+
+COPY .gitignore .npmrc prettier.config.js ./
+COPY package.json package-lock.json turbo.json  ./
+
+COPY apps ./apps
+COPY packages ./packages
+
+RUN npm install
+
+RUN npm run build:mm
+
+RUN npm run db:generate
+
+CMD ["npm", "run", "start:mm"]
